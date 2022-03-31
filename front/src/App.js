b7ecee051b35b066cf01f2c95c4cff10e0ffab3b
Martin-J-Larre <martinjlarre@gmail.com>
2022-03-30 23:24:05 -0400

    #2 react router dom on front

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,43 @@
-// import HomePage from './pages/HomePage';
-// import ProductList from './pages/ProductList';
-// import ProductPage from "./pages/ProductPage";
-// import Login from "./pages/LoginPage";
-// import Register from "./pages/RegisterPage";
-
+import HomePage from './pages/HomePage';
+import ProductList from './pages/ProductList';
+import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
+import Login from "./pages/LoginPage";
+import Register from "./pages/RegisterPage";
 
-
-
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from "react-router-dom";
 
 function App() {
-  return (
-    // <HomePage />
-    // <ProductList />
-    // <ProductPage />
-    // <Register />
-    // <Login />
-    <CartPage />
-  );
+    const user = true;
+    return (
+        <Router>
+            <Switch>
+                <Route exact path='/'>
+                    <HomePage />
+                </Route>
+                <Route path='/products/:category'>
+                    <ProductList />
+                </Route>
+                <Route path='/product/:id'>
+                    <ProductPage />
+                </Route>
+                <Route path='/cart'>
+                    <CartPage />
+                </Route>
+                <Route path='/login'>
+                    { user ? <Redirect to="/"/> : <Login />}
+                </Route>
+                <Route path='/register'>
+                { user ? <Redirect to="/"/> : <Register /> }
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
